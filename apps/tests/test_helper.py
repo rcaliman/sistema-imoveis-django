0dca02887bccfde621e722b0c9099d756eb575b5
Rodrigo Caliman <rcaliman@gmail.com>
2024-01-17 00:58:34 -0300

    teste helpers

--- apps/tests/test_helper.py
+++ apps/tests/test_helper.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
-from apps.imoveis.models.locador import Locador
-from apps.imoveis.models.cliente import Cliente
-from apps.imoveis.models.imovel import Imovel
+from apps.imoveis.models import Locador, Cliente, Imovel
 from apps.imoveis.forms.locador import FormLocador
 from apps.imoveis.forms.imovel import FormImovel
 from apps.imoveis.forms.cliente import FormCliente
@@ -18,7 +16,7 @@ class TestHelper(TestCase):
             'telefone': '1111111111',
             'email': 'teste@teste.com',
         }
-        FormLocador(self.data_locador).save()
+        self.locador_pk = Locador.objects.create(**self.data_locador).pk
         self.data_cliente = {
             'nome': 'John Doe',
             'data_nascimento': '1978-05-17',
@@ -27,30 +25,30 @@ class TestHelper(TestCase):
             'telefone_1': '2799999999',
             'telefone_2': '2799998888',
         }
-        FormCliente(self.data_cliente).save()
+        self.cliente_pk = Cliente.objects.create(**self.data_cliente).pk
         self.data_imovel = {
             'tipo': 'apartamento',
             'numero': '100',
             'local': 'galeria',
-            'cliente': Cliente.objects.get(id=1),
+            'cliente': Cliente.objects.get(id=self.cliente_pk),
             'valor': 100.0,
             'complemento': 'teste de complemento',
             'observacao': 'teste de observacao',
             'dia': 10,
         }
-        FormImovel(self.data_imovel).save()
-        self.data_imovel = {
+        self.imovel_pk = Imovel.objects.create(**self.data_imovel).pk
+        self.data_condominio = {
             'tipo': 'condomínio',
             'numero': '100',
             'local': 'galeria',
-            'cliente': Cliente.objects.get(id=1),
+            'cliente': Cliente.objects.get(id=self.cliente_pk),
             'valor': 100.0,
             'complemento': '',
             'observacao': '',
             'dia': 10,
         }
-        FormImovel(self.data_imovel).save()
-        self.locador = Locador.objects.get(id=1)
+        self.condominio_pk = Imovel.objects.create(**self.data_condominio).pk
+        self.locador = Locador.objects.get(id=self.locador_pk)
         self.registros_imoveis = Imovel.objects.all()
         return super().setUp(*args, **kwargs)
     
