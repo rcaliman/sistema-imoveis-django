16676e6ae6237e979290609ac5efd2db62efd482
Rodrigo Caliman <rcaliman@gmail.com>
2024-01-16 22:06:43 -0300

    ajuste no tipo de campo de Locador

--- apps/imoveis/models/locador.py
+++ apps/imoveis/models/locador.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Locador(models.Model):
-    nome = models.TextField(max_length=200, blank=False, null=False)
-    cpf = models.TextField(max_length=20, blank=True, null=True)
-    residencia = models.TextField(max_length=200, blank=True, null=True)
-    estado_civil = models.TextField(max_length=100, blank=True, null=True)
+    nome = models.CharField(max_length=200, blank=False, null=False)
+    cpf = models.CharField(max_length=20, blank=True, null=True)
+    residencia = models.CharField(max_length=200, blank=True, null=True)
+    estado_civil = models.CharField(max_length=100, blank=True, null=True)
     data_nascimento = models.DateField(max_length=100, blank=True, null=True)
-    telefone = models.TextField(max_length=100, blank=True, null=True)
-    email = models.TextField(max_length=200, blank=True, null=True)
-    nacionalidade = models.TextField(max_length=200, blank=True, null=True)
+    telefone = models.CharField(max_length=100, blank=True, null=True)
+    email = models.CharField(max_length=200, blank=True, null=True)
+    nacionalidade = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.nome
