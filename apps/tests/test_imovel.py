7d4b6f215183ea3673d577c6c56131e321b705c6
Rodrigo Caliman <rcaliman@gmail.com>
2024-05-06 20:06:14 -0300

    ajuste de testes

--- apps/tests/test_imovel.py
+++ apps/tests/test_imovel.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from apps.imoveis.forms.imovel import FormImovel
 from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 from django.urls import reverse
 from apps.imoveis.models.imovel import Imovel
 from apps.imoveis.models.cliente import Cliente
+from apps.imoveis.models.locador import Locador
 
 class TesteImovel(TestCase):
     def setUp(self, *args, **kwargs):
@@ -19,15 +21,29 @@ class TesteImovel(TestCase):
             'cidade_residencia_sede': 'colatina-es',
         }
         self.cliente_pk = Cliente.objects.create(**self.data_cliente).pk
+        self.data_locador = {
+            'nome': 'John Doe',
+            'cpf': '66755631736',
+            'residencia': 'colatina',
+            'estado_civil': 'casado',
+            'data_nascimento': '1978-01-01',
+            'telefone': '1111111111',
+            'email': 'teste@teste.com',
+        }
+        self.locador_pk = Locador.objects.create(**self.data_locador).pk 
         self.data_imovel = {
             'tipo': 'apartamento',
             'numero': '100',
             'local': 'galeria',
-            'cliente': Cliente.objects.get(id=self.cliente_pk),
+            'cliente': Cliente.objects.last(),
             'valor': 100.0,
             'complemento': 'teste de complemento',
             'observacao': 'teste de observacao',
             'dia': 10,
+            'iptu_inscricao': '1234567',
+            'iptu_titular': Locador.objects.last(),
+            'elfsm_inscricao': '1234567',
+            'elfsm_titular': Locador.objects.last(),
         }
         self.imovel_pk = Imovel.objects.create(**self.data_imovel).pk
         self.user = User.objects.create_user('temporario', 'teste@teste.com', 'temporario')
@@ -58,13 +74,11 @@ class TesteImovel(TestCase):
 
     def test_view_lista_imoveis_atualizar(self):
         self.autentica()
-        self.insere_imovel()
         url = reverse('imoveis_lista')
-        self.data_imovel['id'] = self.imovel_pk
-        self.data_imovel['valor'] = 125.5
-        resposta = self.client.post(url, data=self.data_imovel, follow=True)
+        imovel = model_to_dict(Imovel.objects.last())
+        imovel['dia'] = 25
+        resposta = self.client.post(url, data=imovel, follow=True)
         self.assertIn('Imóvel atualizado com sucesso.', resposta.content.decode('utf-8'))
-
 
     def test_view_imovel_formulario_inserir(self):
         self.autentica()
